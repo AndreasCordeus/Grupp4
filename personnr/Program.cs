a9895c518b18f98656b6d561e44ed132a7704759
Andreas <143602115+AndreasCordeus@users.noreply.github.com>
2024-01-17 09:15:27 +0100

    Test update program.cs

--- personnr/Program.cs
+++ personnr/Program.cs
@@ -1,4 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+﻿class Program { 
 
 Console.WriteLine("Hello, Grupp 4!");
+
+KontrollSiffranUträkning.RäknaSistaSiffran();
+    static void Main(string[] args) { 
+
+        HereWeGo.Start();
+
+    }
+
+}
